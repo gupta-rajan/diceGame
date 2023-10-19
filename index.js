db895f22f2215dd42dad510ebaf66d11e909edd3
gupta-rajan <rjngupta22@gmail.com>
2023-10-20 02:11:06 +0530

    update address

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var num1 = Math.floor(Math.random()*6)+1;
 
 var num2 = Math.floor(Math.random()*6)+1;
 
-document.querySelector("div .img1").setAttribute("src","/images/dice"+num1+".png");
-document.querySelector("div .img2").setAttribute("src","/images/dice"+num2+".png");
+document.querySelector("div .img1").setAttribute("src","images/dice"+num1+".png");
+document.querySelector("div .img2").setAttribute("src","images/dice"+num2+".png");
 
 if (num1 > num2){
     document.querySelector("div h1").innerHTML = "Player1 Wins!";
